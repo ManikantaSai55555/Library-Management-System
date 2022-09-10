502c0a74349e82f20a336e1b09f3881255764298
manikaspa.18@gmail.com <vamshi.bobbiligama@comakeit.com>
2022-09-10 13:42:04 +0530

    files added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using LibraryManagement;
+class Program
+{   public static void Main()
+    {
+        System.Console.WriteLine("\t\t----- Welcome to the Library -----");
+        LibraryManage library=new LibraryManage();
+        library.Start();
+        SQLConnection.sql.Close();
+    }
+}
